d09c3ba52fe59bd592251a202fa0bc7a815d8404
Sarthakkashyap1731 <kashyapsarthak177@gmail.com>
2021-10-27 09:23:40 +0530

    2 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  name = 'Sarthak Kashyap';
+  DOB = '07-31-1999';
+  number = '9826992097';
+  email = 'kashyapsarthak177@gmail.com';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
