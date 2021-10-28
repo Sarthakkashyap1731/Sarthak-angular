7a7e13ffce75b939aa6dafb45dec706003cba62e
Sarthakkashyap1731 <kashyapsarthak177@gmail.com>
2021-10-28 15:23:26 +0530

    1 file modified

--- src/app/header/number.pipe.ts
+++ src/app/header/number.pipe.ts
@@ -4,17 +4,17 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'numberPipe',
 })
 export class NumberPipe implements PipeTransform {
-  transform(allLocation: any, number: any): String {
+  transform(number: any, allLocation: any): String {
     console.log('number', number);
     console.log('allLocationa', allLocation);
-    if (number.includes('India') == true) {
-      return '+91' + allLocation;
+    if (allLocation.includes('India') == true) {
+      return '+91' + number;
     }
     if (allLocation.includes('USA') == true) {
       return '+12' + number;
     }
-    if (number.includes('Japna') == true) {
-      return '+23' + allLocation;
+    if (allLocation.includes('Japna') == true) {
+      return '+23' + number;
     }
     if (allLocation.includes('China') == true) {
       return '+45';
